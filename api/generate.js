32697c81819a93c37d6b67b1b48b003205d7a396
bhuy2808 <bhuy2808@gmail.com>
2026-04-08 10:18:14 +0700

    Add files via upload

--- api/generate.js
+++ api/generate.js
@@ -20,9 +20,8 @@ export default async function handler(req, res) {
         version: "a07f252abbbd832009640b27f063ea52d87d7a23a185ca165bec23b5adc8deaf",
         input: {
           image: `data:image/jpeg;base64,${imageBase64}`,
-          prompt: prompt || "a smiling person, 3d cartoon style, sticker",
-          style: req.body.style && ["3D", "Clay", "Video game", "Emoji", "Toy", "Pixel art"].includes(req.body.style) ? req.body.style : "Clay",
-          instant_id_strength: 0.65
+          prompt: prompt,
+          instant_id_strength: 0.6
         }
       })
     });
